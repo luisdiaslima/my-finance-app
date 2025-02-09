fd2022761d5214f0b22ca3ef8fb589761c568527
Luis <luisdiaslima25@gmail.com>
2025-02-08 21:30:43 -0300

    feat: remove nun used attribute

--- src/components/NaturalForm/Component.tsx
+++ src/components/NaturalForm/Component.tsx
@@ -115,7 +115,6 @@ export const NaturalForm = () => {
                 id="name"
                 name="name"
                 type="text"
-                test-id
                 onChange={handleChange}
                 onBlur={validateName}
                 className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm/6 text-gray-900 focus:outline-none focus:ring-2 focus:ring-sky-700"
